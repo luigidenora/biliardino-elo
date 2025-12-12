25865ca5e3786ebe788045df7bd1d902cf9db308
ca-agargaro <agargaro@codearchitects.com>
2025-12-12 22:27:36 +0100

    fix: correct input path for add.html in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(config => ({
       input: {
         main: path.resolve(__dirname, 'index.html'),
         players: path.resolve(__dirname, 'players.html'),
-        moligarg: path.resolve(__dirname, 'moligarg.html')
+        add: path.resolve(__dirname, 'add.html')
       }
     }
   }
