652afb46676d2ebe4ee6a80f08f6a3dcda3fa7f2
ca-agargaro <agargaro@codearchitects.com>
2025-12-14 20:50:08 +0100

    feat: remove matchmaking service and enhance play card disclaimer styles for responsiveness

--- src/ranking.ts
+++ src/ranking.ts
@@ -1,5 +1,4 @@
 import { MatchService } from './services/match.service';
-import { MatchmakingService } from './services/matchmaking.service';
 import { PlayerService } from './services/player.service';
 import { RepositoryService } from './services/repository.service';
 import { updateElo } from './utils/update-elo.util';
@@ -11,31 +10,3 @@ PlayerService.loadPlayers(await RepositoryService.loadPlayers());
 MatchService.getAllMatches().forEach(m => updateElo(m));
 
 RankingView.init();
-
-console.time('Matchmaking Time');
-console.log(MatchmakingService.findBestMatches([
-  'Andrea Gargaro',
-  'Samuele Pesce',
-  'Davide Silletti',
-  'Alfredo Sette',
-  'Francesco Molinari',
-  'Loris Bevilacqua',
-  'Michele Porcu',
-  'Michele Lillo',
-  'Salvatore Defino',
-  'Dario Spinosa',
-  'Matteo Attanasio',
-  'Andrea Fraccalvieri',
-  'Nicola Sergio',
-  'Filippo Addabbo',
-  'Luigi Denora',
-  'Giuseppe Latrofa',
-  'Davide Colucci',
-  'Domenico Pace',
-  'Andrea Difonzo',
-  'Michele Sette',
-  'Andrea Greco',
-  'Gianluca De Vincenzo',
-  'Marco De Luca'
-]));
-console.timeEnd('Matchmaking Time');
