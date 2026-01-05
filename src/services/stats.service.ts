2f0258f9b942065e21636673c6f942df1501f989
ca-agargaro <agargaro@codearchitects.com>
2026-01-05 14:53:00 +0100

    feat: iterate matches in reverse order for improved performance in getPlayerStats function

--- src/services/stats.service.ts
+++ src/services/stats.service.ts
@@ -80,7 +80,7 @@ export function getPlayerStats(player: number): PlayerStats {
   let bestVictoryScore = -1;
   let worstDefeatScore = -1;
 
-  for (const match of matches) {
+  for (const match of matches.toReversed()) { // TODO iterate it reversed instead of create new array
     const team = getTeam(player, match);
     if (team === -1) continue;
 
